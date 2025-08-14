5b174946e884eae3d3270598d3663c30bd3ef05e
KaiSundararaj <82894667+KaiSundararaj@users.noreply.github.com>
2025-08-13 20:52:08 -0400

    Added intro section

---
+++ script.js
@@ -0,0 +1,11 @@
+function initialize(){
+   const aboutMe = document.getElementById("aboutMe");
+   aboutMe.innerHTML += "Hello! I'm Kai, a 4th year computer science student at Carleton University graduating in May 2026."
+   aboutMe.innerHTML += " I am currently finishing a coop as a software engineer at Ericsson, and ";
+   aboutMe.innerHTML += " I am currently looking for new opportunities in software development."
+   aboutMe.innerHTML += " In my free time, I enjoy working on personal projects, playing badminton, and reading manga.";
+   aboutMe.innerHTML += " Feel free to check out some of my projects below, or to connect with me on Linkedin.";
+   
+}
+
+window.onload = initialize;
